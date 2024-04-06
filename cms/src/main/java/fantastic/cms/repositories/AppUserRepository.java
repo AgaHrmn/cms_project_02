eda636208abaf0a2d6095511b9731300b006ea21
EloMorelo <jakub0zietek@gmail.com>
2024-04-06 14:34:59 +0200

    Usuniecie AppUser etc

--- cms/src/main/java/fantastic/cms/repositories/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package fantastic.cms.repositories;
-
-import fantastic.cms.models.AppUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AppUserRepository extends JpaRepository<AppUser, String> {
-    AppUser findByUsername(String username);
-}
